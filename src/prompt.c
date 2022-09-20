faa2e458b70ac0b987eeec22024e2a3d0e1689ef
dguet <dguet@student.42angouleme.fr>
2022-09-20 17:36:15 +0200

    feat(prompt): modify readline call + add history file

--- src/prompt.c
+++ src/prompt.c
@@ -3,21 +3,21 @@
 int	prompt_shell(void)
 {
 	char	*buffer;
-	size_t	buf_size;
+//	size_t	buf_size;
 
-	buf_size = 2048;
-	buffer = (char *)malloc(sizeof(char) * buf_size);
+//	buf_size = 2048;
+	/*buffer = (char *)malloc(sizeof(char) * buf_size);
 	if (!buffer)
 	{
 		perror("Malloc failure");
 		return (1);
-	}
+	}*/
 	while ((buffer = readline("Mickeytotal$>")) != NULL)
 	{
 		printf("cmd = %s\n", buffer);
 		write(1, "$> ", 3);
+		free(buffer);
 	}
 	printf("\nHave a nice day with MickeyTotal \n");
-	free(buffer);
 	return (0);
 }
