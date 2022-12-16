e1432acc076d74272d8cb6828fd07ae76d083610
dguet <dguet@student.42angouleme.fr>
2022-12-16 13:21:27 +0100

    feat: init commit

---
+++ src/heredoc/heredoc.c
@@ -0,0 +1,13 @@
+#include "minishell.h"
+
+////GET HEREDOC/////////
+
+
+
+////////////////////////
+
+void	ft_heredoc(t_pipes_data *pipes_data, char **envp_l)
+{
+	//get_heredoc();
+	//interpret_env_variables();
+}
